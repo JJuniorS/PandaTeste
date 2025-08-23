ccab0541d6dc1d8c4380221d61137014402b22b1
JJuniorS <myannotation.dev@gmail.com>
2025-08-23 18:31:00 -0300

    new class viagem

---
+++ pandaTeste.api/Controllers/ViagensController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pandaTeste.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ViagensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ pandaTeste.api/Dtos/Viagem.cs
@@ -0,0 +1,12 @@
+﻿namespace pandaTeste.api.Dtos
+{
+    public class Viagem
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; } = "";
+        public DateTime DtViagem { get; set; }
+        public string Destino { get; set; } = "";
+        public decimal Preco { get; set; } = 0.00m;
+        public decimal Orcamento { get; set; } = 0.00m;
+    }
+}
